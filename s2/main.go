c9fd89a2250765072341b8956310d3cd18c511c4
l <luis@MacBook-Air-de-l.local>
2018-07-23 12:14:50 +0200

    +go fmt s2

--- s2/main.go
+++ s2/main.go
@@ -8,28 +8,28 @@ import "strconv"
 import "encoding/json"
 
 type result struct {
-    Lat float64 `json:"Lat"`
-    Lon float64 `json:"Lon"`
+	Lat float64 `json:"Lat"`
+	Lon float64 `json:"Lon"`
 }
 
-func s2toLonLat(w http.ResponseWriter, r *http.Request){
+func s2toLonLat(w http.ResponseWriter, r *http.Request) {
 	strCell := r.FormValue("cell")
 	fmt.Println(strCell)
-	cell,_ := strconv.ParseUint(strCell,0,64)
+	cell, _ := strconv.ParseUint(strCell, 0, 64)
 	ll := s2.CellID(cell).LatLng()
 
 	res := result{
 		Lat: ll.Lat.Degrees(),
-		Lon: ll.Lng.Degrees() }
-	
+		Lon: ll.Lng.Degrees()}
+
 	json.NewEncoder(w).Encode(res)
 }
 
 func handleRequests() {
-    http.HandleFunc("/", s2toLonLat)
-    log.Fatal(http.ListenAndServe(":8081", nil))
+	http.HandleFunc("/", s2toLonLat)
+	log.Fatal(http.ListenAndServe(":8081", nil))
 }
 
 func main() {
-    handleRequests()
+	handleRequests()
 }
